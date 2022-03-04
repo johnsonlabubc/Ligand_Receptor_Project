4961c5384d081b359174322338f5b5b02ccdacb0
Sep Kamal <sepehr.kamal@gmail.com>
2022-03-04 00:26:42 -0800

    continue on omnipath ligands list and categories

--- ligand_receptor_lists/feb2022_new_lists/OmniPath/omnipath_explore.R
+++ ligand_receptor_lists/feb2022_new_lists/OmniPath/omnipath_explore.R
@@ -26,8 +26,7 @@ print_interactions(head(interactions))
 
 
 # ligand-receptor interactions without literature reference
-interactions_extra <- import_ligrecextra_interactions(resources=c("iTALK",
-                                                            "Baccin2019"), organism=9606)
+interactions_extra <- import_ligrecextra_interactions(organism=9606)
 
 View(interactions_extra)
 # can see there are 3013 extra ligand-receptor interactions
@@ -41,9 +40,12 @@ write_tsv(interactions_extra, "ligand_receptor_lists/feb2022_new_lists/OmniPath/
 ## We check some of the different intercell categories
 get_intercell_generic_categories()
 
+get_intercell_categories()
 
 ## We import the intercell data into a dataframe
 intercell <- import_omnipath_intercell() 
+
+
   
 nrow(intercell)
 # we see that there are 323572 intercellular communication 
@@ -81,13 +83,22 @@ as.data.frame(intercell_ligrec$genesymbol) %>%
 
 # lets try just getting ligands, and filter out complexes
 intercell_ligands <- import_omnipath_intercell(categories = "ligand",
-                                               resources = "OmniPath",
+                                               source = "composite",
                                                secreted = TRUE) %>% 
                      filter(entity_type == "protein")
 # 1479 ligands, including complexes
+# tried changing to aspect = functional & scope = specific
+# but both resulted in 0 genes being returned
 
 nrow(intercell_ligands)
-# 897 lgands, not including complexes
+# 897 ligands, not including complexes
+
+as.data.frame(intercell_ligands$uniprot) %>% 
+  distinct() %>% 
+  nrow()
+# confirmed that all 897 ligands are unique
+# NOTE: use uniprot for joins/merge's cause a few don't have genesymbol
+
 
 intercell_ligands %>% 
   group_by(plasma_membrane_transmembrane) %>% 
@@ -101,8 +112,165 @@ as.data.frame(intercell_ligands$transmitter) %>%
 # confirmed that all ligands are transmitters, are secreted, and are not receivers
 
 
+# In the intercell database a `consensus_score` is available for each composite 
+# record: the number of resources annotating the protein for a certain category
 
-intercell_filter <- intercell_consensus_filter(intercell,
-                                               percentile = )
+# noted that some ligands from ligrec_extra are not in the 897 ligand list
+# check to see if they are in the cell_surface_ligand list
+intercell_all_ligands <- import_omnipath_intercell(categories = c("ligand",
+                                                                     "cell_surface_ligand"),
+                                               source = "composite",
+                                               secreted = TRUE) %>% 
+  filter(entity_type == "protein")
+# this added about 120 genes, of which 45 were unique, so total is now 1036
+as.data.frame(intercell_all_ligands$uniprot) %>% 
+  distinct() %>% 
+  nrow()
+
+
+# compare list to ligrec_extra
+extra_ligands <- interactions_extra %>% 
+  select(source) %>% 
+  distinct()
+# has 1651 unique ligands, though 30 of them are complexes
+
+# filter out the complexes
+extra_ligands <- extra_ligands %>% 
+  filter(substr(source, 1, 3) != "COM")
+# left with 1621 extra ligands
+
+# check how many of the main list ligands are in the extra ligands
+intercell_all_ligands$uniprot %in% extra_ligands$source %>% 
+  summary()
+# 866 are in extra list, 170 are not
+
+# now check opposite (how many extra ligands are in our main list)
+extra_ligands$source %in% intercell_all_ligands$uniprot %>% 
+  summary()
+# 775 are in it, 846 are not
+
+### lets continue for now without the extra ligands
+ligands_df <- intercell_all_ligands %>% 
+  select(category,
+         uniprot,
+         genesymbol,
+         consensus_score) #%>% 
+#  filter(consensus_score > 2)
+# the 30 cell surface ligands all have a consensus score <=4
+
+### also get table of all intercell signalling categories genes for reference
+intercell_all <- import_omnipath_intercell(source = "composite") %>%
+                   filter(entity_type == "protein")
+# gives us 53,000 genes
+
+# check how many duplicate genes
+as.data.frame(intercell_all$uniprot) %>% 
+  distinct() %>% 
+  nrow()
+# 20,925 unique genes
+
+# save table of all interaction categories genes
+write_tsv(intercell_all, "ligand_receptor_lists/feb2022_new_lists/OmniPath/data/allcategories_intercell_genes.tsv")
+
+
+# check all categories in our table
+as.data.frame(intercell_all$category) %>% 
+  distinct()
+
+# create table of just the key categories from fig 2 in their paper
+intercell_main <- import_omnipath_intercell(categories = c("adhesion",
+                                                           "receptor",
+                                                           "ligand",
+                                                           "ecm",
+                                                           "secreted_enzyme",
+                                                           "cell_surface_enzyme",
+                                                           "cell_surface_ligand",
+                                                           "transporter"),
+                                            source = "composite") %>% 
+                   filter(entity_type == "protein")
+# gives us 8887 genes 
+
+# check how many duplicate genes
+as.data.frame(intercell_main$uniprot) %>% 
+  distinct() %>% 
+  nrow()
+# 6271 unique genes here
+
+# save table of main interaction categories genes
+write_tsv(intercell_main, "ligand_receptor_lists/feb2022_new_lists/OmniPath/data/maincategories_intercell_genes.tsv")
+
+
+### try to append all the categories that our ligand genes have to our ligands list
+### in 1 column
+
+# first do with all categories
+# widen our datatable using reshape2
+intercell_all_cat <-intercell_all %>% 
+  select(uniprot,
+         category) %>% 
+  reshape2::dcast(uniprot ~ category, function(x) any(length(x)))
+
+# replace TRUE's with column names
+intercell_final <- intercell_all_cat %>% 
+  gather(key = "key", value = "value", -uniprot) %>%  #Change to long format
+  filter(value) %>% # Filter for value which are TRUE
+  group_by(uniprot) %>% 
+  summarise(omnipath_categories = paste0(key,collapse=", ")) 
+
+
+## repeat with just the main categories
+
+# widen our datatable using reshape2
+intercell_main_cat <- intercell_main %>% 
+  select(uniprot,
+         category) %>% 
+  reshape2::dcast(uniprot ~ category, function(x) any(length(x)))
+
+# replace TRUE's with column names
+intercell_main_final <- intercell_main_cat %>% 
+  gather(key = "key", value = "value", -uniprot) %>%  #Change to long format
+  filter(value) %>% # Filter for value which are TRUE
+  group_by(uniprot) %>% 
+  summarise(omnipath_main_categories = paste0(key,collapse=", ")) 
+
+
+## append both the new categories columns to our ligand list
+
+# remove duplicates in ligand list
+ligands_df <- ligands_df %>% 
+  select(uniprot, genesymbol, consensus_score) %>% 
+  # distinct based on uniprot only (to remove duplicates with diff consensus scores)
+  distinct(uniprot, .keep_all = TRUE)
+# brings us from 1036 ligands (with duplicates) to 942
+
+# join tables
+ligands_with_cat <- ligands_df %>% 
+  left_join(intercell_main_final,
+            by = "uniprot") %>% 
+  left_join(intercell_final,
+            by = "uniprot") %>% 
+  rename(generic_categories = omnipath_main_categories) %>% 
+  rename(all_categories = omnipath_categories) 
+
+# save ligands table
+write_tsv(ligands_with_cat, "ligand_receptor_lists/feb2022_new_lists/OmniPath/data/ligands.tsv")
+
+
+
+## now get annotations for our ligands
+get_annotation_resources()
+annotation_categories()
+
+ligands_with_cat$uniprot %>% 
+translate_ids(organism = 9606,
+              uniprot_id = uniprot,
+              esnt)
+
+  
+
+import_omnipath_annotations(c("INS", "INSR"),
+                            resources = "KEGG-PC",
+                            wide = TRUE)
+
 
 View(intercell_ligands)
